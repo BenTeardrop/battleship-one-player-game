999df1b8053807c5ccb8e58b451177daa4b0708f
Ben Jim <jimenezbenjamin@yahoo.com>
2022-04-13 13:12:56 +0000

    Initial commit, add players board and computers board

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+board = [('0') * 5 for x in range(5)]
+computer_board = [('0') * 5 for x in range(5)]
+
+
+def print_board(board):
+    """
+    Prints the board 5 x 5 coordinates
+    and an horinzontal line 
+    to separate the two boards
+    """
+    print ('---------')
+    for row in board:
+        print(" ".join(row))
+    print ('---------')
+
+
+
+
+print('BATTLESHIP')
+print_board(board)
+print_board(computer_board)
